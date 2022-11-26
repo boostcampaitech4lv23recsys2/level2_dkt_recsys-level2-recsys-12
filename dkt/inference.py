c257745cebf23ec9d465faee04f7aa7ec4da259d
김다은_T4022 <de021@naver.com>
2022-11-26 13:52:08 +0900

    Revert "Remove: dkt folder"
    This reverts commit 0bf9d0d98c53118169aea5475cd93245d6ce882c.

---
+++ dkt/inference.py
@@ -0,0 +1,30 @@
+import os
+
+import torch
+from args import parse_args
+from src import trainer
+from src.dataloader import Preprocess
+
+
+def main(args):
+    args.device = "cuda" if torch.cuda.is_available() else "cpu"
+    preprocess = Preprocess(args)
+    preprocess.load_test_data(args.test_file_name)
+    test_data = preprocess.get_test_data()
+    # model = trainer.get_model(args).to(args.device)
+    if not args.kfold:
+        model = trainer.load_model(args).to(args.device)
+        trainer.inference(args, test_data, model)
+    else:
+        for fold in range(args.kfold):
+            print(f"Inference... fold {fold}")
+            model = trainer.load_model_kfold(args, fold).to(
+                args.device
+            )  # fold번째 pt 파일 불러옴
+            trainer.inference_kfold(args, test_data, model, fold)  # pt 파일 가지고 inference
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    os.makedirs(args.model_dir, exist_ok=True)
+    main(args)
---
+++ dkt/train.py
@@ -0,0 +1,37 @@
+import os
+
+import torch
+import wandb
+from args import parse_args
+from src import trainer
+from src.dataloader import Preprocess
+from src.utils import setSeeds
+
+
+def main(args):
+    wandb.login()
+
+    setSeeds(args.seed)
+    args.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    preprocess = Preprocess(args)
+    preprocess.load_train_data(args.file_name)
+    train_data = preprocess.get_train_data()
+
+    wandb.init(project="DKT_DKT", config=vars(args), entity="ai-tech-4-recsys-12")
+    wandb.name = (
+        f"{args.model}_{args.n_epochs}_{args.batch_size}_{args.lr}_{args.patience}"
+    )
+    model = trainer.get_model(args).to(args.device)
+
+    if not args.kfold:
+        train_data, valid_data = preprocess.split_data(train_data)
+        trainer.run(args, train_data, valid_data, model)
+    else:
+        trainer.run_kfold(args, train_data, preprocess, model)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    os.makedirs(args.model_dir, exist_ok=True)
+    main(args)
