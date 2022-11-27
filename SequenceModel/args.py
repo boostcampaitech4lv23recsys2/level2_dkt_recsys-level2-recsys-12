aa1d3e87318354e53c36ee56afd28bde0c843256
JisooRyu99 <cussion09@gmail.com>
2022-11-28 01:41:04 +0900

    Fix : Fix typo

--- SequenceModel/args.py
+++ SequenceModel/args.py
@@ -73,10 +73,10 @@ def parse_args():
 
     ### Data Augmentation ###
     parser.add_argument(
-        "--window", default=False, type=bool, help="Data Augmentation / True or False"
+        "--window", default=False, type=bool, help="Data Augmentation window / True or False"
     )
     parser.add_argument(
-        "--shuffle", default=False, type=bool, help="Data Augmentation / True or False"
+        "--shuffle", default=False, type=bool, help="Data Augmentation shuffle / True or False"
     )
     parser.add_argument("--stride", default=20, type=int, help="Data Augmentation stride") # default = max_seq_len
     parser.add_argument("--shuffle_n", default=2, type=int, help="Data Augmentation shuffle")
