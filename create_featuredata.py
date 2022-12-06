9072a34ca0a4fbb368736cbe8eb13d6fe17e0f02
7dudtj <7dudtj@naver.com>
2022-12-06 14:23:41 +0900

    Fix: add test_data.csv at create_featuredata.py

--- create_featuredata.py
+++ create_featuredata.py
@@ -2,6 +2,8 @@ import pandas as pd
 import feature_engineering as fe
 
 train_data = pd.read_csv("../data/train_data.csv")
+test_data = pd.read_csv("../data/test_data.csv")
 featured_train_data = fe.feature_engineering(train_data)
-featured_train_data = featured_train_data
+featured_test_data = fe.feature_engineering(test_data)
 featured_train_data.to_csv("../data/featured_train_data.csv", index=False)
+featured_test_data.to_csv("../data/featured_test_data.csv", index=False)
